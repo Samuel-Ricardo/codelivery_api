d793618770cea79c6bb42b6404d0c576a44437f6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-02 13:51:45 -0300

    :sparkles: setup: modules - App

--- codelivery_api/src/app.module.ts
+++ codelivery_api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DNS, { useNewUrlParse: true }),
+    RoutesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
