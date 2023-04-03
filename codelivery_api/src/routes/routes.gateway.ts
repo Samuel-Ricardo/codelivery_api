e9e0395ed6bdc462097168c74f024baa81dafe80
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-03 10:27:02 -0300

    :sparkles: create: constructor - RoutesGateway

--- codelivery_api/src/routes/routes.gateway.ts
+++ codelivery_api/src/routes/routes.gateway.ts
@@ -1,7 +1,19 @@
-import { OnModuleInit } from "@nestjs/common";
-import { WebSocketGateway } from "@nestjs/websockets";
+import { Inject, OnModuleInit } from "@nestjs/common";
+import { ClientKafka } from "@nestjs/microservices";
+import { Producer } from "@nestjs/microservices/external/kafka.interface";
+import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from "socket.io";
 
 @WebSocketGateway()
 export class RoutesGateway implements OnModuleInit {
+  private kafkaProducer: Producer;
+
+  @WebSocketServer()
+  server: Server;
+
+  constructor(
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient: ClientKafka,
+  ){}
   
 }
