dc90085fed1dec63c63ed5a477a8fe8ee48f8e25
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-16 15:50:00 -0300

    :construction_worker: credentials (src/routes/routes.module.ts)

--- codelivery_api/src/routes/routes.module.ts
+++ codelivery_api/src/routes/routes.module.ts
@@ -22,6 +22,12 @@ import { RoutesGateway } from './routes.gateway';
             client: {
               clientId: process.env.KAFKA_CLIENT_ID,
               brokers: [process.env.KAFKA_BROKER],
+              ssl: true,
+              sasl: {
+                mechanism: 'plain',
+                username : process.env.KAFKA_SASL_USERNAME,
+                password : process.env.KAFKA_SASL_PASSWORD,
+              } 
             },
             consumer: {
               groupId:
